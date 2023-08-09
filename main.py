327fee7ea260e3c7512d9e7627862aad0bc2fff4
Artyom0chka <artem140107@gmail.com>
2023-08-09 19:10:59 +0300

    Изменил код, поменяв кнопки

--- main.py
+++ main.py
@@ -6,24 +6,23 @@ bot = telebot.TeleBot('6379030655:AAFJ-mDOfa2hb_nsNCGE2dWla8AkKBiBQrA')
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, f'Хей, мальчишка  *{message.from_user.first_name}*', parse_mode="Markdown")
-    markup01 = types.InlineKeyboardMarkup()
-    button01 = types.InlineKeyboardButton(text='ДА!', callback_data='delete')
+    markup01 = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    button01 = types.KeyboardButton('ДА!')
     markup01.add(button01)
-    bot.send_message(message.chat.id, 'Хочешь похвастаться сакцесом?', reply_markup=markup01)
+    bot.send_message(message.chat.id, text=f'Хей, мальчишка *{message.from_user.first_name}*, Хочешь похвастаться сакцесом?', parse_mode="Markdown", reply_markup=markup01)
 
-@bot.callback_query_handler(func=lambda call: True)
-def callback_inline(callback):
-   if callback.data == 'delete':
-       markup02 = types.InlineKeyboardMarkup()
-       button02 = types.InlineKeyboardButton(text='Ретродроп', callback_data='retro')
-       button03 = types.InlineKeyboardButton(text='Сладкий флип', callback_data='flip')
-       markup02.add(button02,button03)
-       bot.send_message(callback.message.chat.id, 'Каким именно?', reply_markup=markup02)
+@bot.message_handler(content_types=['text'])
+def func(message):
+    if(message.text == 'ДА!'):
+        markup02 = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn02 = types.KeyboardButton("Ретродроп")
+        btn03 = types.KeyboardButton("Сладкий флип")
+        markup02.add(btn02,btn03)
+        bot.send_message(message.chat.id, text='Каким именно?', reply_markup=markup02)
+        bot.register_next_step_handler(message, on_click)
+def on_click(message):
+    if message.text == "Ретродроп" or "Сладкий флип":
+        bot.send_message(message.chat.id, text='Отправь свой кошелек')
 
-@bot.callback_query_handler(func=lambda call: True)
-def callback_inline(callback1):
-    if callback1.data == 'retro' or 'flip':
-        bot.send_message(callback1.message.chat.id, 'Отправь свой кошелек')
 
 bot.polling(none_stop=True)
